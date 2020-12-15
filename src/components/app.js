e36752460d7305055d52f1cbb58e38e9cf892d61
Arthur Falcão <arthurjvfalcao@gmail.com>
2020-12-15 17:41:44 +0000

    :rocket: add app element and change index file

---
+++ src/components/app.js
@@ -0,0 +1,24 @@
+import { PolymerElement, html } from "@polymer/polymer";
+import "./add-item";
+import "./list-items";
+
+class TodoApp extends PolymerElement {
+  static get properties() {
+    return {
+      todoList: {
+        type: Array,
+        value: JSON.parse(localStorage.getItem("todo-list")) || [],
+      },
+    };
+  }
+
+  static get template() {
+    return html`
+      <p>Hello World 2</p>
+      <add-item></add-item>
+      <list-items todo-list="{{todoList}}"></list-items>
+    `;
+  }
+}
+
+window.customElements.define("todo-app", TodoApp);
